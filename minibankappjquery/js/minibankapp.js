80753c4cd02833626223b84611a5dcb81249a6e7
LACHHAB AYOUB <44031876+LACHHABAYOUB@users.noreply.github.com>
2021-04-08 21:01:33 -0500

    Add files via upload

--- minibankappjquery/js/minibankapp.js
+++ minibankappjquery/js/minibankapp.js
@@ -1,3 +1,5 @@
+var tblAccounts = document.getElementById("tblAccounts").getElementsByTagName('tbody')[0];
+
 $(document).ready(function () {
   var i = 3; // Next Value Should Be on the Table
 
@@ -17,6 +19,89 @@ $(document).ready(function () {
     document.getElementById("txtCustomerName").value = "";
     document.getElementById("ddlAccountType").value = "";
 
+
+    
   });
+
+  // saving 
+const SavingBank = document.getElementById("SavingBank");
+SavingBank.addEventListener('change', function (event) {
+event.preventDefault();
+
+const tr = tblAccounts.getElementsByTagName("tr");
+
+for (let i = 0; i < tr.length; i++) {
+
+  let td = tr[i].getElementsByTagName("td")[3];
+  console.log(td.textContent)
+
+    if (this.checked && td.textContent === "Checking" ) {
+        tr[i].style.display = "none";
+
+    } 
+    else if (this.checked && td.textContent === "Loan" ){
+      tr[i].style.display = "none";
+
+    }
+    else {
+      
+      tr[i].style.display = "";
+
+    }
+}
+
 });
 
+// checking 
+  const CheckingBank = document.getElementById("CheckingBank");
+  CheckingBank.addEventListener('change', function (event) {
+  event.preventDefault();
+  
+  const tr = tblAccounts.getElementsByTagName("tr");
+  
+  for (let i = 0; i < tr.length; i++) {
+  
+    let td = tr[i].getElementsByTagName("td")[3];
+    console.log(td.textContent)
+  
+      if (this.checked && td.textContent === "Savings") {
+          tr[i].style.display = "none";
+      }else if (this.checked && td.textContent === "Loan" ){
+        tr[i].style.display = "none";
+  
+      }
+       else {
+          tr[i].style.display = "";
+  
+      }
+  }
+  
+  });
+
+// loan 
+
+  const LoanBank = document.getElementById("LoanBank");
+  LoanBank.addEventListener('change', function (event) {
+  event.preventDefault();
+  
+  const tr = tblAccounts.getElementsByTagName("tr");
+  
+  for (let i = 0; i < tr.length; i++) {
+  
+    let td = tr[i].getElementsByTagName("td")[3];
+    console.log(td.textContent)
+  
+      if (this.checked && td.textContent === "Savings") {
+          tr[i].style.display = "none";
+      }else if (this.checked && td.textContent === "Checking" ){
+        tr[i].style.display = "none";
+  
+      } else {
+          tr[i].style.display = "";
+  
+      }
+  }
+  
+  });
+
+});
